bd599fe6e92d00fa53a32bbb883e5cbaf7e0a0ff
Ankit <ankitnig123@gmail.com>
2023-05-21 12:00:50 +0530

    add: initial files

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../style'
+
+const Button = () => (
+  <button className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-[10px]`} type='button'>Get Started</button>
+)
+
+
+export default Button
